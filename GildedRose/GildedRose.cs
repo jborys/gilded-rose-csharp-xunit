89cdd45d50dc34c55d2120bd5d4e2722aed625f3
jborys <jborys@Jindustriallogic.com>
2024-07-18 12:03:08 -0400

    initial commit

---
+++ GildedRose/GildedRose.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+
+namespace GildedRoseKata;
+
+public class GildedRose
+{
+    private readonly IList<Item> Items;
+
+    public GildedRose(IList<Item> Items)
+    {
+        this.Items = Items;
+    }
+
+    public void UpdateQuality()
+    {
+        foreach (var item in Items) UpdateItemQuality(item);
+    }
+
+    private void UpdateItemQuality(Item item)
+    {
+        switch (item.Name)
+        {
+            case "Aged Brie":
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality += 1;
+                }
+                else
+                {
+                    if (item.Quality > 0)
+                        item.Quality -= 1;
+                }
+
+                item.SellIn -= 1;
+
+                if (item.SellIn < 0 && item.Quality < 50)
+                {
+                    item.Quality += 1;
+                }
+
+                break;
+            }
+            case "Backstage passes to a TAFKAL80ETC concert":
+            {
+                if (item.Quality < 50)
+                {
+                    item.Quality += 1;
+                    if (item.SellIn < 11)
+                        if (item.Quality < 50)
+                            item.Quality += 1;
+
+                    if (item.SellIn < 6)
+                        if (item.Quality < 50)
+                            item.Quality += 1;
+                }
+
+                item.SellIn -= 1;
+
+                if (item.SellIn < 0)
+                    item.Quality = 0;
+                break;
+            }
+            case "Sulfuras, Hand of Ragnaros":
+                break;
+            default:
+            {
+                if (item.Quality > 0)
+                    item.Quality -= 1;
+
+                item.SellIn -= 1;
+
+                if (item.SellIn < 0 && item.Quality > 0)
+                {
+                    item.Quality -= 1;
+                }
+
+                break;
+            }
+        }
+    }
+}
